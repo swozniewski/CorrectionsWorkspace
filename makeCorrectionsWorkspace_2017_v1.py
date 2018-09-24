92356a8e65522bc3b65097f44468340d711662fc
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2018-09-24 21:56:33 +0100

    adding single electron 27 OR 32 (singleEG version) OR 35 scale factors

--- makeCorrectionsWorkspace_2017_v1.py
+++ makeCorrectionsWorkspace_2017_v1.py
@@ -196,7 +196,10 @@ histsToWrap = [
     (loc+'2017/Ele24/electron_SFs.root:embed_trg_eff', 'e_trg24_embed'),
     (loc+'2017/Ele24/fromDoubleE/electron_SFs.root:data_trg_eff', 'e_trg24_fromDoubleE_data'),
     (loc+'2017/Ele24/fromDoubleE/electron_SFs.root:ZLL_trg_eff', 'e_trg24_fromDoubleE_mc'),
-    (loc+'2017/Ele24/fromDoubleE/electron_SFs.root:embed_trg_eff', 'e_trg24_fromDoubleE_embed')
+    (loc+'2017/Ele24/fromDoubleE/electron_SFs.root:embed_trg_eff', 'e_trg24_fromDoubleE_embed'),
+    (loc+'2017/SingleElectron_27_32single_35/electron_SFs.root:data_trg_eff','e_trg_27_32_35_data'),
+    (loc+'2017/SingleElectron_27_32single_35/electron_SFs.root:ZLL_trg_eff','e_trg_27_32_35_mc'),
+    (loc+'2017/SingleElectron_27_32single_35/electron_SFs.root:embed_trg_eff','e_trg_27_32_35_embed')
 ]
 for task in histsToWrap:
     wsptools.SafeWrapHist(w, ['e_pt', 'expr::e_abs_eta("TMath::Abs(@0)",e_eta[0])'],
@@ -222,9 +225,9 @@ for t in ['data', 'mc']:
 w.factory('expr::e_idiso_embed("@0*@1", e_id_embed, e_iso_embed)')
 w.factory('expr::e_idiso_binned_embed("@0*@1", e_id_embed, e_iso_binned_embed)')
 
-for t in ['trg', 'trg24', 'trg24_fromDoubleE', 'trg_binned', 'id', 'iso', 'iso_binned', 'idiso_binned', 'id_pog', 'idiso_pog', 'looseid_pog', 'looseidiso_pog' ]:
+for t in ['trg', 'trg24', 'trg_27_32_35', 'trg24_fromDoubleE', 'trg_binned', 'id', 'iso', 'iso_binned', 'idiso_binned', 'id_pog', 'idiso_pog', 'looseid_pog', 'looseidiso_pog' ]:
     w.factory('expr::e_%s_ratio("@0/@1", e_%s_data, e_%s_mc)' % (t, t, t))
-for t in ['trg', 'trg24', 'trg_binned', 'id', 'iso', 'iso_binned', 'idiso_binned']:
+for t in ['trg', 'trg24', 'trg_27_32_35', 'trg_binned', 'id', 'iso', 'iso_binned', 'idiso_binned']:
     w.factory('expr::e_%s_embed_ratio("@0/@1", e_%s_data, e_%s_embed)' % (t, t, t))
 
 
